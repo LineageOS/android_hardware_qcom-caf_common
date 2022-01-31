6e77f4fa119b1fe1dca6606b37865aa784d7bf7f
Alexander Koskovich <akoskovich@pm.me>
2022-01-30 18:04:49 -0700

    common: Include QSSI & vendor or just QSSI SEPolicy.
    Change-Id: Ib18f0150db76fb150c0cd258b74de99e4b95d282

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,6 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# SELinux
+ifneq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+else
+include device/qcom/sepolicy/SEPolicy.mk
+endif
+
 # WiFi
 ifneq (,$(filter init, $(PRODUCT_QTI_COMPONENTS)))
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
