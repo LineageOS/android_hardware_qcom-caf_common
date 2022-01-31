b4c00ea45ed20f4419940d70f6e967206e11d583
Alexander Koskovich <akoskovich@pm.me>
2022-01-30 17:44:25 -0700

    common: Import common QTI scripts from LA.VENDOR.1.0.r1-11000-WAIPIO.0.
    Change-Id: I05f2455ff81681e6b299eb299fa837aa643785b7

---
+++ common.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Components
+ifeq ($(PRODUCT_QTI_COMPONENTS), all)
+PRODUCT_QTI_COMPONENTS := \
+    init
+endif
+
+# Initialization
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.mdm.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.rc \
+    init.qcom.sh \
+    ueventd.qcom.rc
+
+ifneq (,$(filter init, $(PRODUCT_QTI_COMPONENTS)))
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)/init
+endif
