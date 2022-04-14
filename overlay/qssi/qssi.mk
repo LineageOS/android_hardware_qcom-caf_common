ba4b3022fc8d30ea4fa6c555930e887ea5542c5c
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 20:41:13 -0700

    qcom: Import cleaned QSSI RROs from LE2115.11.C.48
     * Remove anything that the value is already the default,
       or deprecated.
    
    Change-Id: I597a93b580754a7fde51d051accfffca54c22482

---
+++ overlay/qssi/qssi.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    FrameworksResCommon \
+    TelephonyResCommon \
+    WifiResCommon
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
