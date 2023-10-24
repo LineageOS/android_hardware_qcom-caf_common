0379343d09592239d21809e5e24eacfde19bbb21
Bruno Martins <bgcngm@gmail.com>
2023-10-25 00:49:33 +0100

    Introduce qcom_defs
    The main place to set Qualcomm platform name variables.
    
    Change-Id: I25beb9fb2105bcccb3f272eba45b6cca01659615

---
+++ qcom_defs.mk
@@ -0,0 +1,13 @@
+# Platform name variables - used in makefiles everywhere
+MSMSTEPPE := sm6150
+TRINKET := trinket #SM6125
+
+# UM families
+UM_3_18_FAMILY := msm8996 msm8937 msm8953
+UM_4_4_FAMILY := msm8998 sdm660
+UM_4_9_FAMILY := sdm845 sdm710
+UM_4_14_FAMILY := msmnile $(MSMSTEPPE) $(TRINKET) atoll
+UM_4_19_FAMILY := kona lito bengal
+UM_5_4_FAMILY := lahaina holi
+UM_5_10_FAMILY := taro parrot
+UM_5_15_FAMILY := kalama
