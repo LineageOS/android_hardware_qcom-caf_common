65bfe6c151bc3c6df81eda8c3e374b2ac5281e03
LuK1337 <priv.luk@gmail.com>
2023-12-06 09:54:05 +0100

    common: Add liboemcrypto.so to vendor linker config
    This lets com.google.android.widevine.apex access it.
    
    Change-Id: I4e9654fe6317b722f5fbf43b92abb367c18640ce

--- common.mk
+++ common.mk
@@ -1,3 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+# Linker config
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    $(LOCAL_PATH)/linker.config.json
+
 # RFS APQ GNSS symlinks
 PRODUCT_PACKAGES += \
     rfs_apq_gnss_hlos_symlink \
