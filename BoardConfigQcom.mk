02153e69af6f14dc09c7b7d75948d2419c5cbc1c
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:30:43 +0200

    qcom: Remove no longer used TARGET_USES_DRM_PP
    Not used after display HAL blueprint conversion
    
    Change-Id: Ib4f706bd685d12164d4d99db05ccce4f04dcc48d

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -171,7 +171,6 @@ endif
 # Enable DRM PP driver on UM platforms that support it
 ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     SOONG_CONFIG_qtidisplay_drmpp := true
-    TARGET_USES_DRM_PP := true
 endif
 
 # Enable Gralloc4 on UM platforms that support it
