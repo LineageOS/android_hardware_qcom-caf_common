5691b167423a153fec9f0b87bb75cfa06a41c5c5
Arian <arian.kulmer@web.de>
2025-02-18 17:37:09 +0100

    qcril-database: Add host tool to generate qcrilNr.db from migrations
    Change-Id: Icf597968b3ae7bbb614510693167296c0a14d82e

---
+++ qcril-database/generate_qcril_db.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2024-2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+import os
+import re
+import sqlite3
+import sys
+
+
+def version_prefix(file_path):
+    basename = os.path.basename(file_path)
+    match = re.match(r'(\d+(\.\d+)?)', basename)
+    # Split the version to properly sort subversions
+    return [int(part) for part in match.group(0).split('.')] if match else []
+
+
+def main():
+    if len(sys.argv) <= 2:
+        print('syntax: generate_qcril_db.py target_db sql_file0 sql_file1...')
+        sys.exit(1)
+
+    target_db = sys.argv[1]
+    sql_files = sys.argv[2:]
+
+    if os.path.exists(target_db):
+        os.remove(target_db)
+
+    # Split the config sql and ecc sql files
+    config_sql_files = [
+        file for file in sql_files if file.endswith('_config.sql')
+    ]
+    ecc_sql_files = [
+        file for file in sql_files if not file.endswith('_config.sql')
+    ]
+
+    config_sql_files.sort(key=version_prefix)
+    ecc_sql_files.sort(key=version_prefix)
+
+    ordered_migrations = ecc_sql_files + config_sql_files
+
+    with sqlite3.connect(target_db) as conn:
+        cursor = conn.cursor()
+        for file in ordered_migrations:
+            try:
+                with open(file, 'r') as sql_file:
+                    sql_script = sql_file.read()
+                    cursor.executescript(sql_script)
+            except Exception as e:
+                print(f'Failed to apply {file}: {e}')
+                sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
