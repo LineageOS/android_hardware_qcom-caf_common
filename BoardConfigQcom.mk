867f7ca992a9447df6b0733f91350dccc9edafa7
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-24 13:30:05 -0500

    Allow legacy devices to use /firmware for symlinks
    Change-Id: Ie5b071908765e80618de780fc31620f798d845cf

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -205,11 +205,11 @@ SOONG_CONFIG_NAMESPACES += rfs
 
 # Add supported variables to rfs config
 SOONG_CONFIG_rfs += \
-    mpss_firmware_symlink_target \
+    firmware_symlink_target \
     persist_symlink_target
 
 # Set default values for rfs config
-SOONG_CONFIG_rfs_mpss_firmware_symlink_target ?= firmware_mnt
+SOONG_CONFIG_rfs_firmware_symlink_target ?= firmware_mnt
 SOONG_CONFIG_rfs_persist_symlink_target ?= mnt_vendor_persist
 
 # Add rmnetctl to soong config namespaces
