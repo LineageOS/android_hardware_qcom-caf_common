13b40876e38b5346dddf2e3afd06c7b2869f3980
Edwin Moquete <edwinmmoquete@gmail.com>
2025-07-14 13:49:41 +0000

    qcom: Conditionally use sm8250 HALs for sdm845
    sdm845 devices running kernel 4.19 will use sm8250 HALs.
    
    Change-Id: I11e37df1726b5dbbe53a1463c87c8e3fabe89f64

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -357,7 +357,11 @@ else ifneq ($(filter $(UM_4_4_HAL_FAMILY),$(TARGET_BOARD_PLATFORM)),)
 else ifneq ($(filter $(UM_4_19_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     QCOM_HARDWARE_VARIANT := sdm660
 else ifneq ($(filter $(UM_4_9_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sdm845
+    ifneq ($(TARGET_KERNEL_VERSION), 4.19)
+        QCOM_HARDWARE_VARIANT := sdm845
+    else
+        QCOM_HARDWARE_VARIANT := sm8250
+    endif
 else ifneq ($(filter $(UM_4_14_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     QCOM_HARDWARE_VARIANT := sm8150
 else ifneq ($(filter $(UM_4_19_FAMILY),$(TARGET_BOARD_PLATFORM)),)
