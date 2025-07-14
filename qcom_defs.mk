7b50cd08907bc8d85ea45af681e88f213b107f84
Edwin Moquete <edwinmmoquete@gmail.com>
2025-07-14 13:49:41 +0000

    qcom: Move sdm845 to UM_4_19_FAMILY
    Change-Id: I11e37df1726b5dbbe53a1463c87c8e3fabe89f64

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -5,9 +5,9 @@ TRINKET := trinket #SM6125
 # UM families
 UM_3_18_FAMILY := msm8996 msm8937 msm8953
 UM_4_4_FAMILY := msm8998 sdm660
-UM_4_9_FAMILY := sdm845 sdm710
+UM_4_9_FAMILY := sdm710
 UM_4_14_FAMILY := msmnile $(MSMSTEPPE) $(TRINKET) atoll
-UM_4_19_FAMILY := kona lito bengal
+UM_4_19_FAMILY := kona lito bengal sdm845
 UM_5_4_FAMILY := lahaina holi
 UM_5_10_FAMILY := taro parrot
 UM_5_15_FAMILY := kalama crow
