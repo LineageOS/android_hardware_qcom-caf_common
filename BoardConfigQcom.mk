9cf24b2cd885efcbc6c65543f806652128d1d563
Edwin Moquete <edwinmmoquete@gmail.com>
2025-07-14 13:49:41 +0000

    qcom: Conditionally use sm8250 HALs for sdm845
    sdm845 devices running kernel 4.19 will use sm8250 HALs.
    
    Change-Id: I11e37df1726b5dbbe53a1463c87c8e3fabe89f64

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -185,6 +185,7 @@ SOONG_CONFIG_qtidisplay += \
     target_no_raw10_custom_format \
     target_uses_aligned_ycbcr_height \
     target_uses_aligned_ycrcb_height \
+    target_uses_legacy_camera \
     target_uses_unaligned_nv21_zsl \
     target_uses_unaligned_ycrcb \
     target_uses_ycrcb_camera_preview \
@@ -215,6 +216,7 @@ SOONG_CONFIG_qtidisplay_target_kernel_version ?= 0
 SOONG_CONFIG_qtidisplay_target_no_raw10_custom_format ?= false
 SOONG_CONFIG_qtidisplay_target_uses_aligned_ycbcr_height ?= false
 SOONG_CONFIG_qtidisplay_target_uses_aligned_ycrcb_height ?= false
+SOONG_CONFIG_qtidisplay_target_uses_legacy_camera ?= false
 SOONG_CONFIG_qtidisplay_target_uses_unaligned_nv21_zsl ?= false
 SOONG_CONFIG_qtidisplay_target_uses_unaligned_ycrcb ?= false
 SOONG_CONFIG_qtidisplay_target_uses_ycrcb_camera_preview ?= false
@@ -370,7 +372,11 @@ else ifneq ($(filter $(UM_4_19_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     QCOM_HARDWARE_VARIANT := sdm660
 else ifneq ($(filter $(UM_4_9_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_4_9_FAMILY)
-    QCOM_HARDWARE_VARIANT := sdm845
+    ifneq ($(TARGET_KERNEL_VERSION), 4.19)
+        QCOM_HARDWARE_VARIANT := sdm845
+    else
+        QCOM_HARDWARE_VARIANT := sm8250
+    endif
 else ifneq ($(filter $(UM_4_14_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_4_14_FAMILY)
     QCOM_HARDWARE_VARIANT := sm8150
