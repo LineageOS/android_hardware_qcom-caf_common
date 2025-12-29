cd600d7651ae32648ddce111b77ccca22f491609
wHo-EM-i <ehteshammalik18998@gmail.com>
2025-12-30 00:00:37 +0530

    qcom: Enable gralloc4 on 4.19 legacy family
    4.19 legacy family (e.g. sdm660) supports gralloc4, but it
    was not enabled after the display HAL blueprint conversion.
    
    Change-Id: Ia59e810b9982f808a57828125d2066cf061bb1ef
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -242,7 +242,7 @@ ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FA
 endif
 
 # Enable Gralloc4 on UM platforms that support it
-ifneq ($(filter $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY) $(UM_6_1_FAMILY) $(UM_6_6_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY) $(UM_6_1_FAMILY) $(UM_6_6_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     SOONG_CONFIG_qtidisplay_gralloc4 := true
 endif
 
