a044cb6d4683564dc20c163fb849c41bb5643e3c
LuK1337 <priv.luk@gmail.com>
2026-01-14 13:23:57 +0100

    common: Add sm8450-6.6 support
    Change-Id: I0e6f26167019fb63c38d40f223e92ad3372abd65

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -13,3 +13,9 @@ UM_5_10_FAMILY := taro parrot
 UM_5_15_FAMILY := kalama crow
 UM_6_1_FAMILY := pineapple volcano
 UM_6_6_FAMILY := sun
+
+ifeq ($(TARGET_KERNEL_VERSION),6.6)
+# UM 5.10 has been upgraded to UM 6.6
+UM_6_6_FAMILY := $(UM_6_6_FAMILY) $(UM_5_10_FAMILY)
+UM_5_10_FAMILY :=
+endif
