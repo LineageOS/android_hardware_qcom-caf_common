110303449e611c6b11d9676e4920b227636b6f66
Amrita Das <bhabanidas431@gmail.com>
2026-04-21 02:03:24 +0530

    common: add support for blair platform
    Change-Id: I6e413ec9ad80cf67ef34d8d4301120bb231447ae

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -11,7 +11,7 @@ UM_4_19_FAMILY := kona lito bengal
 UM_5_4_FAMILY := lahaina holi
 UM_5_10_FAMILY := taro parrot
 UM_5_15_FAMILY := kalama crow
-UM_6_1_FAMILY := pineapple volcano
+UM_6_1_FAMILY := pineapple volcano blair
 UM_6_6_FAMILY := sun
 
 ifeq ($(TARGET_KERNEL_VERSION),6.6)
